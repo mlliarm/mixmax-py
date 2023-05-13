c4a34c56ffe83db6cc31f742ef4dca9aa42c8ead
milia <milia@pitagoras>
2023-05-13 06:12:51 +0200

    resolved incorrect type issue, see #5

--- src/cython/create_matrix_fast3.pyx
+++ src/cython/create_matrix_fast3.pyx
@@ -27,9 +27,9 @@ import numpy as np
 def create_matrix(unsigned int N, long s):
     cdef double [:,:] A = np.ones(shape=(N,N),dtype=np.float64)
     cdef int i, j, k
-    for i in range(N):
-        for j in range(N):
-            for k in range(N):
+    for i in range(<int> N):
+        for j in range(<int> N):
+            for k in range(<int> N):
                 if i-j == k and i > 0 and j > 0:
                     A[i,j] = 2 + k
     if N > 2:
@@ -39,11 +39,11 @@ def create_matrix(unsigned int N, long s):
 def create_matrix_three_params(unsigned int N, long s, unsigned long m):
     cdef double [:,:] A = np.ones(shape=(N,N),dtype=np.float64)
     cdef int i, j, k
-    for i in range(N):
-        for j in range(N):
+    for i in range(<int> N):
+        for j in range(<int> N):
             if i-j == 0 and i > 0 and j > 0:
                 A[i,j] = 2
-            for k in range(1,N):
+            for k in range(1, <int> N):
                 if i-j == k and i > 0 and j > 0:
                     A[i,j] = 2 + k*m
     if N > 2:
