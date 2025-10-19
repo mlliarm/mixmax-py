48ed013c95f46225928be9d6ec28635cf02f4511
milia <mlliarm@users.noreply.github.com>
2025-10-19 07:26:32 +0200

    update

--- src/cython/run_tests.py
+++ src/cython/run_tests.py
@@ -2,7 +2,7 @@
 #
 # mixmax-py - a PRNG based on uniformly hyperbolic Anosov C-systems defined on a torus.
 #
-# Copyright (C) 2019-2022  Michail Liarmakopoulos <mlliarm@yandex.com>
+# Copyright (C) 2019-2025  Michail Liarmakopoulos <mlliarm@yandex.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
